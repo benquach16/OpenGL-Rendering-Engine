ade2837b3999c61e7c8d29df789c5431241b26f6
Ben Quach <benquach16@yahoo.com>
2017-12-31 05:44:51 -0800

    get texcoords in vertex shader

--- shaders/vs_general.glsl
+++ shaders/vs_general.glsl
@@ -3,7 +3,10 @@
 layout(location = 0) in vec3 vertex_position;
 layout(location = 1) in vec2 tex_coord;
 
+out vec2 tex;
+
 void main()
 {
+	tex = tex_coord;
 	gl_Position=vec4(vertex_position, 1.0);
 }
